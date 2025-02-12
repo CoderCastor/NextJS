309aa24fbcd1709618e2936f8dade704d6767d25
Tejas Shinde <tejasshinde@Tejass-MacBook-Pro.local>
2025-02-13 00:28:32 +0530

    this project is done

--- first-project/app/api/auth/signup/route.ts
+++ first-project/app/api/auth/signup/route.ts
@@ -1,12 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
 
-export async function POST(req : NextRequest){
-    
-    const data = await req.json()
-    console.log(data)
+const prismaClient = new PrismaClient();
 
-    return NextResponse.json({
-        message:"You have been signup",
-        data
-    })
+export async function POST(req: NextRequest) {
+    const data = await req.json();
+    console.log(data);
+    console.log("Hi bois");
+
+  await prismaClient.user.create({
+      data:{
+          username : data.username,
+          password : data.password
+      }
+  })
+
+  return NextResponse.json({
+    message: "You have been signup",
+  });
 }
--- first-project/app/auth/signup/page.tsx
+++ first-project/app/auth/signup/page.tsx
@@ -1,34 +1,53 @@
-"use client"
-import axios from 'axios'
-import React, { useState } from 'react'
-import { useRouter } from "next/navigation"
+"use client";
+import axios from "axios";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 
 function page() {
+  const router = useRouter();
 
-    const router = useRouter()
-
-    const [username,setUsername] = useState("")
-    const [password,setPassword] = useState("")
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <div className='h-screen w-screen flex flex-col justify-center items-center p-10 bg-zinc-900 gap-5'>
-      <div className='flex justify-center flex-col gap-5 rounded-2xl px-28 py-16 items-center bg-zinc-800'>
-      <h2 className='text-4xl text-white'>Sign Up</h2>
-      <input onChange={e => {
-        setUsername(e.target.value)
-      }} className='bg-zinc-900 rounded-2xl px-2 py-2' type="text" name="username" placeholder='username' id="" />
-      <input onChange={e => {
-        setPassword(e.target.value)
-      }} className='bg-zinc-900 rounded-2xl px-2 py-2' type="password" name="password" placeholder='password' id="" />
-      <button onClick={()=>{
-        axios.post('http://localhost:3000/api/auth/signup',{
-            username,password
-        })
+    <div className="h-screen w-screen flex flex-col justify-center items-center p-10 bg-zinc-900 gap-5">
+      <div className="flex justify-center flex-col gap-5 rounded-2xl px-28 py-16 items-center bg-zinc-800">
+        <h2 className="text-4xl text-white">Sign Up</h2>
+        <input
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+          className="bg-zinc-900 rounded-2xl px-2 py-2"
+          type="text"
+          name="username"
+          placeholder="username"
+          id=""
+        />
+        <input
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          className="bg-zinc-900 rounded-2xl px-2 py-2"
+          type="password"
+          name="password"
+          placeholder="password"
+          id=""
+        />
+        <button
+          onClick={() => {
+            axios.post("http://localhost:3000/api/auth/signup", {
+              username,
+              password,
+            });
 
-        router.push('/auth/signin')
-      }}>Sign up</button>
+            // router.push("/auth/signin");
+            console.log("frontend")
+          }}
+        >
+          Sign up
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
